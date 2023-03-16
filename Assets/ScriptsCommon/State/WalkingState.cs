fcf32c360f8ab6f6b5660f5e3b436a7ae46fb222
tiennq <tiennguyenhe153125@gmail.com>
2023-03-16 17:19:23 +0700

    Update state

--- Assets/ScriptsCommon/State/WalkingState.cs
+++ Assets/ScriptsCommon/State/WalkingState.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class WalkingState : BaseState
 {
+    private void RunToMain(float speed, Creep creep)
+    {
+        creep.stepPerFrame = Time.fixedDeltaTime * speed;
+        creep.gameObject.transform.position = Vector2.MoveTowards(creep.transform.position, creep.main.transform.position, creep.stepPerFrame);
+    }
     public override void EnterState(Creep creep)
     {
         creep.speedAction = creep.type.Speed;
@@ -18,8 +23,10 @@ public class WalkingState : BaseState
         throw new System.NotImplementedException();
     }
 
-    //public override void UpdateState(Creep creep)
-    //{
-    //    throw new System.NotImplementedException();
-    //}
+    public override void UpdateState(Creep creep)
+    {
+        RunToMain(creep.speedAction, creep);
+        Vector2 facingDirection = creep.main.transform.position - creep.transform.position;
+        ScreenHelper.Facing(facingDirection, creep.isFacingRight, creep.gameObject);
+    }
 }
