11b71ac83d2fdcc04919fab188b61ae32290b11e
nguyenkhanhhuyhe153585 <huynkhe153585@fpt.edu.vn>
2023-03-17 08:51:37 +0700

    Chỉnh lại Animator cho Creep

--- Assets/ScriptsCommon/State/WalkingState.cs
+++ Assets/ScriptsCommon/State/WalkingState.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class WalkingState : BaseState
 {
-    private void RunToMain(float speed, Creep creep)
-    {
-        creep.stepPerFrame = Time.fixedDeltaTime * speed;
-        creep.gameObject.transform.position = Vector2.MoveTowards(creep.transform.position, creep.main.transform.position, creep.stepPerFrame);
-    }
     public override void EnterState(Creep creep)
     {
         creep.speedAction = creep.type.Speed;
@@ -28,5 +23,11 @@ public class WalkingState : BaseState
         RunToMain(creep.speedAction, creep);
         Vector2 facingDirection = creep.main.transform.position - creep.transform.position;
         ScreenHelper.Facing(facingDirection, creep.isFacingRight, creep.gameObject);
+    }
+
+    private void RunToMain(float speed, Creep creep)
+    {
+        creep.stepPerFrame = Time.fixedDeltaTime * speed;
+        creep.gameObject.transform.position = Vector2.MoveTowards(creep.transform.position, creep.main.transform.position, creep.stepPerFrame);
     }
 }
